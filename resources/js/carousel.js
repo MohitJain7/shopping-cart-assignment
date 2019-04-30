9a55f34a52e89580034af69d60f6fc091510570d
Mohit Jain <mohjain6@publicisgroupe.net>
2019-04-30 11:19:31 +0530

    code update

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -18,8 +18,6 @@ window.carousel = function(slides, dotsElem, dots, isDotElem, prevElem, nextElem
     }
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
-    }
-    for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(' active', '');
     }
     slides[slideIndex - 1].style.display = 'block';
